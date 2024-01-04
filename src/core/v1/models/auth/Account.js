d4d973caf147e55e7dd6c09ff2d2225904549c71
Caleb Nwaizu <calebchris000@gmail.com>
2024-01-04 12:47:37 +0100

    Remove default values

--- src/core/v1/models/auth/Account.js
+++ src/core/v1/models/auth/Account.js
@@ -4,7 +4,6 @@ const sequelize = require("../../../../../app");
 const Account = sequelize.define("Account", {
   id: {
     type: DataTypes.UUID,
-    defaultValue: UUIDV4,
     primaryKey: true,
   },
   userId: {
