e33705f0aa8cfd080563d7d99f01fb3d9466daa5
Caleb Nwaizu <calebchris000@gmail.com>
2024-01-17 10:21:20 +0100

    Setup education

--- src/v1/enums/Enums.js
+++ src/v1/enums/Enums.js
@@ -1,5 +1,31 @@
 const Role = ["jobseeker", "recruiter", "notset"];
-
+const Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const LevelOfStudy = {
+  HS: "High School",
+  AA: "Associate Degree",
+  BA: "Bachelor's Degree",
+  MA: "Master's Degree",
+  PHD: "Doctoral Degree",
+  HND: "Higher National Diploma",
+  MSC: "Master of Science",
+  SSCE: "Senior Secondary Certificate Examination",
+  ND: "National Diploma",
+};
 module.exports = {
   Role,
+  Months,
+  LevelOfStudy,
 };
---
+++ src/v1/models/Education.js
@@ -0,0 +1,86 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../core/database/init");
+const { Months } = require("../enums/Enums");
+
+const Education = sequelize.define(
+  "Education",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+    },
+    schoolName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "School name is required" },
+      },
+    },
+
+    userId: {
+      type: DataTypes.UUID,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    titleOfDegree: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: "Title of degree is required"}
+      }
+    },
+    levelOfStudy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Level of Study is required" },
+      },
+    },
+    degree: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Degree is required" },
+      },
+    },
+    fieldOfStudy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Field of study is required" },
+      },
+    },
+    graduationMonth: {
+      type: DataTypes.ENUM(...Months),
+      allowNull: false,
+      validate: {
+        notNull: { msg: "The month of graduation is required" },
+      },
+    },
+    graduationYear: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isInt: {
+          msg: "Year must be an integer",
+        },
+        min: {
+          args: [1900],
+          msg: "Unless you're a time traveler, graduation year cannot be less than 1900.",
+        },
+        max: {
+          args: [new Date().getFullYear()],
+          msg: "Unless you're a time traveler, graduation year cannot be later than the current year.",
+        },
+      },
+    },
+  },
+  {
+    tableName: "educations",
+    underscored: false,
+  }
+);
+
+module.exports = Education;
